9b826124662701aca00fb8dbe9437d268341accb
Harsh Singh <hisingh1@gmail.com>
2014-04-22 21:36:51 -0500

    refactor: prep db and quizlet modules for database integration
    - rename get_cursor to get_connection in db.py and update quizlet.py
      call sites
    - reformat quizletdeck.py pseudocode into indented snake_case stubs
      (get_keyword_id, set_quizlet_json, ...) ready for real MySQL calls

--- app/quizletdeck.py
+++ app/quizletdeck.py
@@ -1,45 +1,49 @@
 
-def getKeywordId(input):
-keyword_id=mysql.query("SELECT keyword_id FROM keywords WHERE keyword=%s",%(input,))
-if (!keyword_id):
-  mysql.query("INSERT INTO keywords VALUES(keyword) (%s);",%(input,))
-  keyword_id=mysql.query("SELECT keyword_id FROM keywords WHERE keyword=%s",%(input,))
-return keyword_id
+def get_keyword_id(input):
+  keyword_id = mysql.query("SELECT keyword_id FROM keywords WHERE keyword=%s", % (input,))
+  if (!keyword_id):
+    mysql.query("INSERT INTO keywords VALUES(keyword) (%s);", % (input,))
+    keyword_id = mysql.query("SELECT keyword_id FROM keywords WHERE keyword=%s", % (input,))
+  return keyword_id
 
-def getFetchDate(keyword_id):
-date=mysql.query("SELECT fetchdate FROM keywords WHERE keyword_id=%d",%(keyword_id,))
-return fetchdate
 
-def getParsed(??):
-#don't know how to get the parsed data, probably an http call
+def get_fetch_date(keyword_id):
+  date = mysql.query("SELECT fetchdate FROM keywords WHERE keyword_id=%d", % (keyword_id,))
+  return fetchdate
 
-def setQuizletJson(parsed):
-for quizlet_id, json in parsed:
-  mysql.query("INSERT INTO QuizletDecks VALUES(quizlet_id,json) (%s, %s);" %(quizlet_id,json)
-  #suppose to have a certain list of quizlet_id's that we insert
-  #and a bunch of jsons
 
-def setQuizletKeyword(keyword_id,parsed):
-for quizlet_id in parsed.iterkeys():
-  mysql.query("INSERT INTO KeywordsQuizletDecks VALUES(keyword_id,quizlet_id) (%d,%d);",%(keyword_id,quizlet_id))
+def get_parsed(??):
+  # don't know how to get the parsed data, probably an http call
+  pass
+
+
+def set_quizlet_json(parsed):
+  for quizlet_id, json in parsed:
+    mysql.query("INSERT INTO QuizletDecks VALUES(quizlet_id,json) (%s, %s);" %(quizlet_id,json)
+    # suppose to have a certain list of quizlet_id's that we insert
+    # and a bunch of jsons
+
+def set_quizlet_keyword (keyword_id,parsed):
+  for quizlet_id in parsed.iterkeys():
+    mysql.query("INSERT INTO KeywordsQuizletDecks VALUES(keyword_id,quizlet_id) (%d,%d);",%(keyword_id,quizlet_id))
 
 def getQuizletDecks(input):
-keyword_id=getKeywordId(input)
-fetchdate=getFetchDate(keyword_id)
-listOfQid=mysql.query("SELECT quizlet_id FROM KeywordsQuizletDecks WHERE keyword_id=%d",%(keyword_id,))
-if((!listOfQid) || fetchdate>7):
-  parsed=getParsed(??)
-  setQuizletJson(parsed)
-  setQuizletKeyword(keyword_id,parsed)
+  keyword_id=get_keyword_id(input)
+  fetchdate=get_fetch_date(keyword_id)
   listOfQid=mysql.query("SELECT quizlet_id FROM KeywordsQuizletDecks WHERE keyword_id=%d",%(keyword_id,))
-  print("Done adding values")
-else:
-  print("Pulling exisiting values...")
-return listOfQid
+  if((!listOfQid) || fetchdate>7):
+    parsed=get_parsed(??)
+    set_quizlet_json(parsed)
+    set_quizlet_keyword(keyword_id,parsed)
+    listOfQid=mysql.query("SELECT quizlet_id FROM KeywordsQuizletDecks WHERE keyword_id=%d",%(keyword_id,))
+    print("Done adding values")
+  else:
+    print("Pulling exisiting values...")
+  return listOfQid
 
 
-Trigger stuff - in QuizletDecks
-if(!quizlet_id)
-  Insert JSON into QuizletDecks
-else:
-  Update JSON in QuizletDecks
+# Trigger stuff - in QuizletDecks
+# if(!quizlet_id)
+#   Insert JSON into QuizletDecks
+# else:
+#   Update JSON in QuizletDecks
