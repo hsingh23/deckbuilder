dcec3f1ecff2e42367efd5b05ebb243794b07d73
Harsh Singh <hisingh1@gmail.com>
2014-04-24 10:33:29 -0500

    attempts to fix cgi script

--- deckbuilder.cgi
+++ deckbuilder.cgi
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 from wsgiref.handlers import CGIHandler
-from yourapplication import app
+import app
 
 CGIHandler().run(app)
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 #!flask/bin/python
-from app import app
-app.run(host='0.0.0.0', port=5000, debug = True)
+if __name__ == "__main__":
+    from app import app
+    app.run(host='0.0.0.0', port=5000, debug = True)
