0ef0fe6a2d4bedb38eca008addd239aa21a75ce4
Harsh Singh <hisingh1@gmail.com>
2014-04-25 17:51:13 -0500

    refactor: consolidate JS/coffee asset bundles, drop unused files
    Rework webassets bundles so dev and prod each inline compiled
    CoffeeScript into gen/dev.js and minified gen/packed.js. Delete
    unused app/forms.py and server.py, fix virtualhost indentation,
    and run the app directly from app/__init__.py.

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,43 @@ app.config.from_object("__config")
 
 assets = Environment(app)
 assets.url = app.static_url_path
-coffee = Bundle("coffee/all.coffee", filters="coffeescript", output="gen/coffee.js")
-js_dev = Bundle("js/jquery-1.11.0.js", "js/underscore.js", "js/handlebars-v1.3.0.js", "js/bootstrap.js", "js/lunr.js",output="gen/devjs.js")
-js = Bundle("js/jquery-1.11.0.min.js", "js/underscore-min.js","js/bootstrap.min.js", "js/handlebars.runtime-v1.3.0.js", "js/lunr.min.js", output="gen/lib.js")
-scss = Bundle("scss/all.scss", filters="scss", output="gen/main.css") 
-assets.register('coffee', coffee)
-assets.register('js_dev', js_dev)
-assets.register('js', js)
+
+all_dev_js = Bundle(
+    Bundle(
+        "js/jquery-1.11.0.js",
+        "js/underscore.js",
+        "js/handlebars-v1.3.0.js",
+        "js/bootstrap.js",
+        "js/lunr.js",
+    ),
+    Bundle(
+        "coffee/all.coffee",
+        filters="coffeescript"
+    ),
+    output='gen/dev.js'
+)
+
+all_js = Bundle(
+    Bundle(
+        "js/jquery-1.11.0.min.js",
+        "js/underscore-min.js",
+        "js/bootstrap.min.js",
+        "js/handlebars.runtime-v1.3.0.js",
+        "js/lunr.min.js",
+    ),
+    Bundle(
+        "coffee/all.coffee",
+        filters="coffeescript"
+    ),
+    filters='jsmin',
+    output="gen/packed.js"
+)
+scss = Bundle("scss/all.scss", filters="scss", output="gen/main.css")
+assets.register('all_dev_js', all_dev_js)
+assets.register('all_js', all_js)
 assets.register('scss', scss)
 
 from app import views
+
+if __name__ == "__main__":
+    app.run(debug=True)
