b13085b4390f9df863b318450edf1b48bdc54cb9
Harsh Singh <hisingh1@gmail.com>
2014-04-25 08:52:35 -0500

    build: switch deployment from CGI to Apache WSGI for DigitalOcean
    Remove deckbuilder.cgi and add deckbuilder.wsgi plus an Apache
    virtualhost (deckjam.com) under /var/www/deckbuilder. Simplify
    server.py to import the app from the app package and drop the
    rotating-file log setup. Ignore venv/ and remove dev.py shebang.

--- server.py
+++ server.py
@@ -1,12 +1,3 @@
-import logging
-from logging.handlers import RotatingFileHandler
-from flask import Flask
-
-app = Flask(__name__)
-
 if __name__ == '__main__':
-    handler = RotatingFileHandler(
-        'deckbuilder.log', maxBytes=10000, backupCount=1)
-    handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
+    from app import app
     app.run()
