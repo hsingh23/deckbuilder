d365dfc944748be7d59ff550ae47ba58b28969d7
Harsh Singh <hisingh1@gmail.com>
2014-04-26 14:34:21 -0500

    fixes backend

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from app import app
 from flask import request, session, g, redirect, url_for, abort, \
     render_template, flash, Response
 from json import dumps as object_to_json, loads as json_to_object
-from quizlet import get_decks, parse_keywords
+from quizlet_backup import get_decks, parse_keywords
 from util import combinations
 
 
@@ -18,6 +18,7 @@ def decks(keywords):
     return Response(response=ret,
                     status=200,
                     mimetype="application/json")
+
 
 @app.route('/combinations', methods=["GET", "POST"])
 def keyword_combinations():
--- create.sql
+++ create.sql
@@ -1,5 +1,7 @@
 START TRANSACTION;
-DROP TABLE IF EXISTS Users, Keywords, QuizletDecks, UsersDecks, KeywordsQuizletDecks, KeywordsUsersDecks;
+SET GLOBAL general_log='OFF';
+DROP TABLE IF EXISTS Users, Keywords, QuizletDecks, UserDecks, KeywordsQuizletDecks, KeywordsUserDecks;
+DROP PROCEDURE create_or_update_quizlet;
 
 CREATE TABLE `Users` (
 `user_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -10,12 +12,12 @@ CREATE TABLE `Users` (
 CREATE TABLE `Keywords` (
 `keyword_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `keyword` varchar(1024) NOT NULL,
-`created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+`last_updated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE = Innodb;
 
 CREATE TABLE `QuizletDecks` (
 `quizlet_id` int UNSIGNED NULL,
-`json` TEXT NULL
+`json` TEXT NULL,
 ) ENGINE = Innodb;
 
 CREATE TABLE `UserDecks` (
@@ -30,18 +32,36 @@ CREATE TABLE `UserDecks` (
 -- Join Tables (Many to Many)
 CREATE TABLE `KeywordsQuizletDecks` (
 `keyword_id` int NOT NULL REFERENCES `Keywords` (`keyword_id`),
-`quizlet_id` int NOT NULL REFERENCES `QuizletDecks` (`quizlet_id`)
+`quizlet_id` int NOT NULL REFERENCES `QuizletDecks` (`quizlet_id`),
+`terms_selected` INT UNSIGNED  DEFAULT 0,
+`times_deck_selected` INT UNSIGNED DEFAULT 0
 ) ENGINE = Innodb;
 
-CREATE TABLE `KeywordsUsersDecks` (
+CREATE TABLE `KeywordsUserDecks` (
 `keyword_id` int NOT NULL REFERENCES `Keywords` (`keyword_id`),
 `deck_id` int NOT NULL REFERENCES `UserDecks` (`deck_id`)
 ) ENGINE = Innodb;
+
+DELIMITER //
+CREATE PROCEDURE create_or_update_quizlet
+(IN new_quizlet_id INT UNSIGNED, new_keyword_id INT UNSIGNED, new_json TEXT)
+MODIFIES SQL DATA
+BEGIN
+IF NOT EXISTS (SELECT quizlet_id FROM QuizletDecks WHERE quizlet_id = new_quizlet_id) 
+THEN
+  INSERT INTO QuizletDecks (quizlet_id, json) VALUES (new_quizlet_id, new_json);
+  INSERT INTO KeywordsQuizletDecks (quizlet_id, keyword_id) VALUES (new_quizlet_id, new_keyword_id);
+ELSE
+  UPDATE QuizletDecks SET json = new_json WHERE quizlet_id = new_quizlet_id;
+END IF;
+END;//
+DELIMITER ;
 COMMIT;
+
 
 -- Users(user_id, name, prefrences)
 -- Keywords(keyword_id, keyword, created)
--- QuizletDecks(quizlet_id, json)
+-- QuizletDecks(quizlet_id, json, terms_selected, times_deck_selected)
 -- UserDecks(deck_id, user_id, created_on, json, longitude, latitude)
 -- KeywordsQuizletDecks(keyword_id, quizlet_id)
 -- KeywordsUsersDecks(keyword_id, deck_id)
